f5cb84c7b3588820d817a09c54f88aff39d7a6ed
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2021-05-31 23:32:57 +0200

    update index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, eaque! Voluptas, laboriosam repellendus. Quia praesentium quaerat, unde totam sed doloribus dolores quis explicabo maxime ipsum accusantium distinctio repudiandae! Facere, dolor!'
     }
 })
