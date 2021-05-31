b25e5dc0a205d8fe41666901e5348abc19eeb117
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2021-06-01 00:25:58 +0200

    update js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = new Vue({
     data: {
         product_title: 'Socks',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, eaque! Voluptas, laboriosam repellendus. Quia praesentium quaerat, unde totam sed doloribus dolores quis explicabo maxime ipsum accusantium distinctio repudiandae! Facere, dolor!',
-        image: './img/vmSocks-green-onWhite.jpg'
+        image: './img/vmSocks-green-onWhite.jpg',
+        inventory: 5,
+
     }
 })
